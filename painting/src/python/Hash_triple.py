6678e6967aded3941867fcf405a68ee8a1db6f14
Daniel Espinosa <ec.dani@gmail.com>
2016-02-10 01:09:57 +0100

    Hash_triple planteado

---
+++ painting/src/python/Hash_triple.py
@@ -0,0 +1,33 @@
+# Consiste en llevar el estado de los pixeles pintados y
+# probar los 3 polígonos. En desarrollo.
+
+# MAIN
+fichero = open('../../in/learn_and_teach.in', 'r')
+salida = open('../../out/Hash_lineas_learn_and_teach_123175.txt', 'w')
+
+datos = fichero.readline() #Esto coge el numero de filas y columnas
+lineasFichero = fichero.readlines() # Esto coge el dibujo en si.
+
+lineasSalida = []
+
+inst = 0
+flaglinea = False
+iniciolinea = 0
+for linea in range(len(lineasFichero)):
+    for columna in range(len(lineasFichero[linea])):
+        if flaglinea == False and lineasFichero[linea][columna] == "#":
+                flaglinea = True
+                iniciolinea = columna
+        elif flaglinea == True and (columna == 800 or lineasFichero[linea][columna] == "."):
+                flaglinea = False
+                inst += 1
+                lineasSalida.append(
+                    'PAINT_LINE {0} {1} {2} {3}\n'.format(str(linea), str(iniciolinea), str(linea), str(columna - 1)))
+
+salida.write('{0}\n'.format(str(inst)))
+for x in lineasSalida:
+    salida.write(x)
+fichero.close()
+salida.close()
+
+
