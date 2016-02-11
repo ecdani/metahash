8c8f3e8f8d35bf74aa30c6e99a04173c490fe6b6
Jose Maria <josemariagarcia95@gmail.com>
2016-02-11 19:40:31 +0100

    Estructuras de datos

--- painting/src/python/Hash_dron1.py
+++ painting/src/python/Hash_dron1.py
@@ -1,35 +1,57 @@
 
 #FUNCIONES
-def lecturaFichero(fichero, lineasFichero, d):
+def lecturaFichero(fichero, diccionario):
+    # Lectura linea inicial
+    linea = fichero.readline().split()
+    diccionario["rows"] = int(linea[0])
+    diccionario["columns"] = int(linea[1])
+    diccionario["drones"] = int(linea[2])
+    diccionario["turns"] = int(linea[3])
+    diccionario["max_load"] = int(linea[4])
+    # Fin Procesamiento linea inicial
+
+    # Lectura de productos y pesos
+    linea = fichero.readline()
+    diccionario["nProducts"] = int(linea)
+    pesoDeCadaProducto = []
+    for peso in fichero.readline().split():
+        pesoDeCadaProducto.append(int(peso))
+    diccionario["weights"] = pesoDeCadaProducto
+    # Fin de productos y pesos
+
+    # Lectura lineas de almacenes. Tenemos un array de datos de almacen
+    numeroAlmacenes = int(fichero.readline())
+    datosAlmacenes = []
+    # Cada posicion del array tiene una lista de dos elementos.
+    #   array[0] = coordenadas en una tupla (x,y)
+    #   array[1] = array con articulos de cada tipo
+    for almacen in range(numeroAlmacenes):
+        datosAlmacen = []
+        cantidadesDeProductos = []
+        linea = fichero.readline().split()
+        coordenadasAlmacen = (int(linea[0]), int(linea[1]))
+        datosAlmacen.append(coordenadasAlmacen)
+        linea = fichero.readline().split()
+        for elem in linea:
+            cantidadesDeProductos.append(int(elem))
+        datosAlmacen.append(cantidadesDeProductos)
+        datosAlmacenes.append(datosAlmacen)
+    
+
+
+
+
+
 
 
 # MAIN
 fichero = open('../../in/busy_day.in', 'r')
 salida = open('../../out/Hash_dron_python.txt', 'w')
 
-#datos = fichero.readline() #Esto coge el numero de filas y columnas
-lineasFichero = [] # Esto los datos
-lecturaFichero(fichero,lineasFichero)
+datosProblema = {}
+lecturaFichero(fichero,datosProblema)
+print(datosProblema)
 
-lineasSalida = []
-
-inst = 0
-flaglinea = False
-iniciolinea = 0
-for linea in range(len(lineasFichero)):
-    for columna in range(len(lineasFichero[linea])):
-        if flaglinea == False and lineasFichero[linea][columna] == "#":
-                flaglinea = True
-                iniciolinea = columna
-        elif flaglinea == True and (columna == 800 or lineasFichero[linea][columna] == "."):
-                flaglinea = False
-                inst += 1
-                lineasSalida.append(
-                    'PAINT_LINE {0} {1} {2} {3}\n'.format(str(linea), str(iniciolinea), str(linea), str(columna - 1)))
-
-salida.write('{0}\n'.format(str(inst)))
-for x in lineasSalida:
-    salida.write(x)
 fichero.close()
 salida.close()
 
