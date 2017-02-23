48802f6cdbaad61df579599af759804dc0317a41
jariazavalverde <joseriazavalverde@hotmail.com>
2017-02-23 19:09:05 +0100

    creado todod

--- gcode17/syntax.py
+++ gcode17/syntax.py
@@ -2,8 +2,15 @@ from gcode17 import parse
 import classes
 
 structure = """
-
+Main = Int Int Int Int Int -> Videos -> 2@Endpoint -> 3@Request | Problem
+Videos = *Int
+Endpoint = Int Int -> 2@Cache | Endpoint
+Cache = Int Int | pair
+Request = Int Int Int | Request
 """
+
+def pair(x,y):
+	return (x,y)
 
 def getProblem(filein):
 	global structure
