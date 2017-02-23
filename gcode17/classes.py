ce0c1561f0123bfc8de639c473e6a150e21f0c29
Daniel Espinosa <ec.dani@gmail.com>
2017-02-23 20:10:47 +0100

    Metodos que pide riasa

--- gcode17/classes.py
+++ gcode17/classes.py
@@ -1,16 +1,38 @@
 class Problem:
     def __init__(self,nvideos,nendpoint,nrequest,ncache,cache_size,  videos, endpoints, requests):
+        self.cache_size = cache_size
         self.nvideos = nvideos # Tal vez no sea necesario
         self.nendpoint = nendpoint # Tal vez no sea necesario
         self.nrequest = nrequest # Tal vez no sea necesario
         self.ncache = ncache # Tal vez no sea necesario
-        self.cache_size = cache_size # Tal vez no sea necesario
+        self.caches = [] # Tal vez no sea necesario
         self.requests = requests
         self.videos = videos
         self.endpoints = []
         for idx, val in enumerate(endpoints):
             val.ID = idx
             self.endpoints.append(val)
+
+        for n in range(ncache):
+            self.caches.append(Cache(cache_size))
+
+        def videoInCache(IDC, IDV):
+            # comprobar si en esa caché está ese video
+            for i in range(self.caches[IDC].videos)
+                if self.caches[IDC].videos[i] == IDV:
+                    return True
+            return False
+
+        def videoFit (IDC,IDV):
+            # Comprobar si el video cabe en la cache
+            return  (self.caches[IDC].size_cache >= self.videos[IDV])
+        
+        def videoPush (IDC, IDV):
+            self.caches[IDC].videos.append(IDV)
+
+        
+
+
 
 class EndPoint:
     def __init__(self,latency_DC,nCaches,caches):
@@ -23,3 +45,9 @@ class Request:
         self.IDV = IDV
         self.IDE = IDE
         self.nRequest = nRequest
+
+class Cache:
+    def __init__ (self,size_cache):
+        self.videos = []
+        self.size_cache = size_cache
+    
