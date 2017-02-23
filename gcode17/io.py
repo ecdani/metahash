9ec8fcdb6b834f32a73e8ae1c2cc5af68503293a
Diego Sanz <diego070795@hotmail.com>
2017-02-23 21:07:52 +0100

    Algoritmo 1 implementado (Resultados positivos)

--- gcode17/io.py
+++ gcode17/io.py
@@ -1,6 +1,6 @@
 from time import time
 
-files = {0:"kittens", 1:"me_at_the_zoo", 2:"trending_today", 3:"videos_words_spreading"}
+files = {0:"kittens", 1:"me_at_the_zoo", 2:"trending_today", 3:"videos_worth_spreading"}
 
 def filein(i):
 	global files
