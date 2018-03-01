4ac64d3b56f03fa006ec096b7d26ced5e490784d
ecdani <ec.dani@gmail.com>
2018-03-01 19:46:26 +0100

    prueba del parser (no tengo pip)

--- defcom18/driver.py
+++ defcom18/driver.py
@@ -14,20 +14,31 @@ class Problema:
     '''
     rows y columns
     '''
-    def __init__(self):
+    def __init__(self,fil,col,coch,viaj,bon,pas):
         # Parsear archivo
+        self.filas = fil
+        self.columnas = col 
+        self.coches = coch
+        self.viajes = viaj
+        self.bonus = bon
+        self.pasos = pas
 
-    def parsear_archivo:
+    def parsear_archivo(self):
         pass
 
-    def escribir_archivo:
+    def escribir_archivo(self):
         pass
 
 class Viaje:
     '''
     '''
-    def __init__(self):
-        pass
+    def __init__(self,xi,yi,xf,yf,ti,tf):
+        self.xinicio = xi
+        self.yinicio = yi
+        self.xfinal = xf
+        self.yfinal = yf
+        self.turno_inicio = ti 
+        self.turno_fin = tf
 
 class Coche:
     '''
@@ -37,7 +48,8 @@ class Coche:
         self.y = 0
 
 def main():
-    problem = parse("input/redundancy.in",s,globals())
+    problem = parse("input/a_example.in",s,globals())
+    print('hey')
 
 if __name__ == '__main__':
     sys.exit(main())
