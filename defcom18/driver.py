017c7bfa50586dbaef3191874b8e9c1ae829c8d0
ecdani <ec.dani@gmail.com>
2018-03-01 19:30:12 +0100

    estructura del fichero parser

--- defcom18/driver.py
+++ defcom18/driver.py
@@ -1,16 +1,8 @@
 from gcode17 import parse
 
 s = """
-Main = Int Int Int Int Int  -> nPesos-> nAlmacenes -> nPedidos | Problem
-nPesos = Int -> Pesos | Take2
-Pesos = *Int
-nAlmacenes = Int -> 1@Ware | Take2
-Ware = Int Int -> Ware2 | Almacen
-Ware2 = *Int
-nPedidos = Int -> 1@Coord | Take2
-Coord = Int Int -> Cant | Pedido
-Cant = Int -> CantsProd | Take2
-CantsProd = *Int
+Main = Int Int Int Int Int Int -> 3@viaje | Problema
+viaje = Int Int Int Int Int Int | Viaje
 """
 
 
