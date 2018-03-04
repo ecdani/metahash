e53f3ac0edaf0cfc3efac02512e9552ec3bcd917
Daniel Espinosa <ec.dani@gmail.com>
2018-03-04 17:58:48 +0100

    optimizacion 49.460.630 (puesto 23)

--- defcom18/driver.extended_round.3.py
+++ defcom18/driver.extended_round.3.py
@@ -4,8 +4,7 @@
 
 import sys
 from gcode17 import parse
-from operator import itemgetter, attrgetter
-import copy
+from operator import  attrgetter
 
 s = """
 Main = Int Int Int Int Int Int -> 4@viaje | Problema
@@ -29,23 +28,19 @@ def tiempo(c, v):  # Tiempo hasta el comienzo
 
 
 class Problema:
-    '''
-    rows y columns
-    '''
-
     def __init__(self, fil, col, coch, viaj, bon, pas, listViaj):
         # Parsear archivo
-        self.filas = fil
-        self.columnas = col
+        #self.filas = fil
+        #self.columnas = col
         self.coches = coch
-        self.viajes = viaj
+        #self.viajes = viaj
         self.bonus = bon
-        self.pasos = pas
+        #self.pasos = pas
         self.listaViajes = listViaj
         self.listaCoches = []
 
         for i in range(0, self.coches):
-            self.listaCoches.append(Coche(i))
+            self.listaCoches.append(Coche())
 
     def escribir_viaje(self, file):
         fichero = open('defcom18/out/' + file + '.out', 'w')
@@ -59,31 +54,26 @@ class Problema:
             fichero.write(x)
         fichero.close()
 
-    def eval(self):  # ¿Como pueden coger viajes que no pueden llegar a término?
+    def eval(self):
         self.score = 0
-        for c in self.listaCoches:  # si el tiempo para empezar es menor...
+        for c in self.listaCoches:
             for i, v in enumerate(c.viajesRecorridos):
-                # Actualiza la situacion del coche al terminar el viaje previo
-                c.update(i-1)
+                c.update(i-1) # Actualiza la situacion del coche al terminar el viaje previo
                 if (dist_ef(c, v) + v.dtrayecto + c.turno) < v.turnoFin:  # Si llego a completar
-                    if (dist_ef(c, v) + c.turno) == v.turnoInicio:  # Si llego al bonus
+                    if (dist_ef(c, v) + c.turno) == v.turnoInicio:  # Si llego al bonus...
                         self.score += v.dtrayecto + self.bonus
-                    else:  # Si no llego al bonus
+                    else:
                         self.score += v.dtrayecto
         print " Score solucion: " + str(self.score),
 
     def solve(self):
-
         for j, v in enumerate(self.listaViajes):  # Asignar ids
-            v.n = j
-        initial_lenght = len(self.listaViajes)
+            v.n = j 
 
-        # De vuelta a la perspectiva desde el coche
+        # De vuelta a la perspectiva desde el coche #♥
         n_viajes_previos = 0
         while len(self.listaViajes) != n_viajes_previos:
             n_viajes_previos = len(self.listaViajes)
-            #print str(len(self.listaViajes)) + "bucle infinito!!"
-            # Ordenar, los coches que acaban antes primeros.
             self.listaCoches = sorted(self.listaCoches, key=attrgetter('turno'), reverse=False)  # de menos a mas
             for i, c in enumerate(self.listaCoches):
                 lista_viajes_disp = []
@@ -93,11 +83,12 @@ class Problema:
                         lista_viajes_disp.append(w)
 
                 lista_viajes_disp = sorted(lista_viajes_disp, key=attrgetter('score'), reverse=True)  # de mas a menos
-                lista_viajes_disp = sorted(lista_viajes_disp, key=attrgetter('dtiempo'), reverse=False)  # de menos a mas
+                lista_viajes_disp = sorted(lista_viajes_disp, key=attrgetter('dtrayecto'), reverse=False) # de menos a mas
+                lista_viajes_disp = sorted(lista_viajes_disp, key=attrgetter('dtiempo'), reverse=False)  # de menos a mas 
+
                 if len(lista_viajes_disp) > 0: 
                     c.addViaje(lista_viajes_disp[0])
                     self.listaViajes.remove(lista_viajes_disp[0])
-
 
 
 class Viaje:
@@ -109,14 +100,8 @@ class Viaje:
         self.yd = yd
         self.turnoInicio = ti
         self.turnoFin = tf
-        self.recorrido = False
-        self.tiempoCocheAsignado = 0
-        self.coche = False
 
-        # Idea: De los viajes viables, no coger el que se haga más rápido, sino el que ofrezca más score.
-        self.score = 0
-        self.ratioTiempoScore = 0
-
+        self.score = 0 # Normalmente el dtrayecto + bonus si lo hay
         self.dtiempo = 0  # Distancia temporal al inicio para un coche dado
         self.dtrayecto = (abs(xi - xd) + abs(yi - yd))
     
@@ -129,6 +114,9 @@ class Viaje:
     def eval(self, c, bonus):
         self.dtiempo = dist_ef(c, self)
         self.score = 0
+        if self.dtrayecto > 5000: # optimización para el d_metropolis
+            self.dtiempo += self.dtiempo*2 # pk al acabar estaremos muy en a tomar por 
+        
         self.score += self.dtrayecto
         if (dist_ef(c, self) + c.turno) == self.turnoInicio:
             self.score += bonus
@@ -138,15 +126,11 @@ class Viaje:
 
 
 class Coche:
-    def __init__(self, numero):
-        self.numero = numero
+    def __init__(self):
         self.x = 0
         self.y = 0
         self.viajesRecorridos = []
-        self.tiempoNec = 0
         self.turno = 0  # turno actual en el que está el coche
-        self.preferencia = 0
-        self.score = 0  # Variable auxiliar
 
     def addViaje(self, v):
         self.viajesRecorridos.append(v)
@@ -166,38 +150,11 @@ class Coche:
             self.x = v.xd
             self.y = v.yd
 
-    def dist(self, xi, yi, xd, yd):
-        return (abs(xi - xd) + abs(yi - yd))
-
-    def viable(self, w):
-        if (self.dist(self.x, self.y, w.xi, w.yi) + w.dtrayecto + self.turno) >= w.turnoFin:
-            return False
-        return True
-
-    def eval(self, w, bonus):
-
-        dAlInicio = self.dist(self.x, self.y, w.xi, w.yi)
-        tAlInicio = abs(w.turnoInicio - self.turno)
-
-        if len(self.viajesRecorridos) > 0:
-            self.preferencia = w.turnoInicio - self.viajesRecorridos[-1].turnoFin
-        else:
-            self.preferencia = w.turnoInicio
-
-        self.score = 0
-        if w.turnoInicio >= self.turno:
-            if dAlInicio > tAlInicio:
-                self.score += dAlInicio
-            else:
-                self.score += tAlInicio
-        else:
-            self.score += dAlInicio
-            self.score += tAlInicio
-
 
 def main():
     score = 0
-    files = ['a_example', 'b_should_be_easy', 'c_no_hurry', 'd_metropolis', 'e_high_bonus']
+    #files = ['d_metropolis']
+    files = ['a_example', 'b_should_be_easy', 'c_no_hurry','d_metropolis', 'e_high_bonus']
     for f in files:
         print "Resolviendo " + f,
         problem = parse("defcom18/input/"+f+".in", s, globals())
