0af4dfa05b1ae82f9ce651c2fdd12f832cbf6aea
Flopi <flopivillagarcia@gmail.com>
2019-02-28 21:47:38 +0100

    .

--- ghash19/plantilla.py
+++ ghash19/plantilla.py
@@ -5,6 +5,7 @@ import sys
 from gcode17 import parse
 from operator import attrgetter
 import time
+import math
 
 
 def time_passed(time_from):
@@ -15,6 +16,9 @@ class Problema:
     def __init__(self, n_fotos, list_fotos):
         self.nFotos = n_fotos
         self.listFotos = list_fotos
+        for index, photo in enumerate(self.listFotos):
+            photo.id = index
+
         self.slideShow = Slideshow()
     # para cada foto de lsit fotos, crear un objeto slide (metiendole la foto con addslide) y meterlo en Slideshow (con addlista)
 
@@ -28,10 +32,72 @@ class Problema:
         self.listFotos.sort(key=lambda x: x.nEtiquetas, reverse=True)
 
     def createSlideShow(self):
-        for photo in self.listFotos:
+        pop_list_photos = self.get_h_photos_list()
+
+        # ordenacion inicial
+        pop_list_photos.sort(key=lambda x: x.nEtiquetas,
+                             reverse=True)  # ¿hay que asignarlo?
+
+        # primera
+        photo_izq = pop_list_photos.pop(0)
+        slide = Slide()
+        slide.add(photo_izq)
+        self.slideShow.addslide(slide)
+
+        for index, photo in enumerate(pop_list_photos):
+            n_tags_a_buscar = math.ceil(photo_izq.nEtiquetas/2)
+            for index2, photo2 in enumerate(pop_list_photos):
+                photo2.score(n_tags_a_buscar)
+            pop_list_photos.sort(key=lambda x: x.score, reverse=True)
+            print(pop_list_photos)
+            photo_dch = pop_list_photos.pop(0)
+
             slide = Slide()
-            slide.add(photo)
+            slide.add(photo_dch)
             self.slideShow.addslide(slide)
+            photo_izq = photo_dch
+
+    def createSlideShow2(self):
+        pop_list_photos = self.get_v_photos_list()
+        tmp_slide_list = list()
+        for index, photo in enumerate(pop_list_photos):
+            photo_1 = pop_list_photos.pop(0)
+            photo_2 = pop_list_photos.pop(0)
+            slide = Slide()
+            slide.add(photo_1)
+            slide.add(photo_2)
+            tmp_slide_list.append(slide)
+
+        pop_list_photos = self.get_h_photos_list()
+        for index, photo in enumerate(pop_list_photos):
+            photo_1 = pop_list_photos.pop(0)
+            slide = Slide()
+            slide.add(photo_1)
+            tmp_slide_list.append(slide)
+        # ordenacion inicial
+        tmp_slide_list.sort(key=lambda x: x.nEtiquetas,
+                            reverse=True)  # ¿hay que asignarlo?
+
+        # primera
+        slide_izq = tmp_slide_list.pop(0)
+        self.slideShow.addslide(slide_izq)
+
+        for index, photo in enumerate(tmp_slide_list):
+            n_tags_a_buscar = math.ceil(slide_izq.nEtiquetas/2)
+            for index2, slide2 in enumerate(tmp_slide_list):
+                slide2.score(n_tags_a_buscar)
+            tmp_slide_list.sort(key=lambda x: x.score, reverse=True)
+            print(tmp_slide_list)
+            slide_dch = tmp_slide_list.pop(0)
+
+            self.slideShow.addslide(slide_dch)
+            slide_izq = slide_dch
+
+    def get_h_photos_list(self):
+        return list(filter(lambda x: x.orientacion == "H", self.listFotos.copy()))
+
+    def get_v_photos_list(self):
+        return list(filter(lambda x: x.orientacion == "V", self.listFotos.copy()))
 
 
 class Foto:
@@ -40,9 +106,16 @@ class Foto:
         self.nEtiquetas = n_etiquetas
         self.listEtiquetas = list_etiquetas
         self.listEtiquetas[-1] = self.listEtiquetas[-1].strip()
+        self.id = 0
+        self.score = 0
 
     def __repr__(self):
-        return str(self.nEtiquetas)
+        return str(self.id)
+
+    def score(self, n_tags_a_buscar):
+        # Cuanto el numero de tags de la foto se desvia mas de la foto actual es peor score. 0 es optimo.
+        # Cuanto MAS, PEOR.
+        self.score = abs(n_tags_a_buscar - self.nEtiquetas)
 
 
 class Slideshow:
@@ -56,9 +129,16 @@ class Slideshow:
 class Slide:
     def __init__(self):
         self.listafotos = list()
+        self.nEtiquetas = 0
 
     def add(self, foto):
         self.listafotos.append(foto)
+        self.nEtiquetas += foto.nEtiquetas
+
+    def score(self, n_tags_a_buscar):
+        # Cuanto el numero de tags de la foto se desvia mas de la foto actual es peor score. 0 es optimo.
+        # Cuanto MAS, PEOR.
+        self.score = abs(n_tags_a_buscar - self.nEtiquetas)
 
 
 def main():
