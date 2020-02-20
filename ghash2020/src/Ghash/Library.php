b442b95a1fcc0ffe4aaffa612ed35b0477ffd7d0
fmacia <fjmaciaespin@gmail.com>
2020-02-20 19:39:03 +0100

    [UPD] Parseo de archivos

--- ghash2020/src/Ghash/Library.php
+++ ghash2020/src/Ghash/Library.php
@@ -1,8 +1,25 @@
 <?php
 
+namespace Ghash;
+
 class Library {
   public $totalBooks;
   public $daysToSign;
   public $booksPerDay;
   public $books;
+  public $id;
+
+  public function __construct($totalBooks, $daysToSign, $booksPerDay, $books, $id) {
+    $this->totalBooks = $totalBooks;
+    $this->daysToSign = $daysToSign;
+    $this->booksPerDay = $booksPerDay;
+    $this->books = $books;
+    $this->id = $id;
+
+    // echo "Total de libros: $this->totalBooks" . PHP_EOL;
+    // echo "Días para firmar: $this->daysToSign" . PHP_EOL;
+    // echo "Libros por día: $this->booksPerDay" . PHP_EOL;
+    // echo "Libros:" . PHP_EOL;
+    // print_r($this->books);
+  }
 }
--- ghash2020/src/Ghash/Problem.php
+++ ghash2020/src/Ghash/Problem.php
@@ -9,12 +9,45 @@ class Problem {
   public static $totalLibraries;
   public static $totalDays;
   public static $bookScores;
+  public $libraries = [];
 
   public function parse(string $filename): self {
     $handle = fopen($filename, 'r');
+
+    // 1ª línea
+    $line = fgets($handle);
+    list($this->totalBooks, $this->totalLibraries, $this->totalDays) = explode(' ', $line);
+
+    // 2ª línea
+    $line = fgets($handle);
+    $this->bookScores = explode(' ', $line);
+
+    // echo "Total de libros: $this->totalBooks" . PHP_EOL;
+    // echo "Total de bibliotecas: $this->totalLibraries" . PHP_EOL;
+    // echo "Total de días: $this->totalDays" . PHP_EOL;
+    // echo "Puntuaciones de los libros:" . PHP_EOL;
+    // print_r($this->bookScores);
+
+    $lineNumber = 3;
+    $libraryId = 0;
     while (($line = fgets($handle)) !== false) {
-      // TODO: Parseo aquí
+      // Datos biblioteca
+      if ($lineNumber % 2 == 1) {
+        list($libraryTotalBooks, $libraryDaysToSign, $libraryBooksPerDay) = explode(' ', $line);
+
+        $lineNumber++;
+        continue;
+      }
+
+      // Libros en la biblioteca
+      $libraryBooks = explode(' ', $line);
+
+      // Instanciar biblioteca
+      $libraries[] = new Library($libraryTotalBooks, $libraryDaysToSign, $libraryBooksPerDay, $libraryBooks, $libraryId);
+      $lineNumber++;
+      $libraryId++;
     }
+
     fclose($handle);
 
     return $this;
@@ -25,11 +58,11 @@ class Problem {
   }
 
   public function writeSolution(string $filename): self {
-    $handle = fopen($filename, 'w');
+    // $handle = fopen($filename, 'w');
 
     //fwrite($handle, 'meh');
 
-    fclose($handle);
+    // fclose($handle);
 
     return $this;
   }
