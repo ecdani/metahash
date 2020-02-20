66120235e42f258ffc918905fbb5dffec10298f0
Flopi <flopivillagarcia@gmail.com>
2020-02-20 20:45:19 +0100

    huhuhuhuh

--- ghash2020/src/Ghash/Library.php
+++ ghash2020/src/Ghash/Library.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ghash;
 
 class Library {
@@ -8,6 +7,7 @@ class Library {
   public $booksPerDay;
   public $books;
   public $id;
+  public $bookTotalScore
 
   public function __construct($totalBooks, $daysToSign, $booksPerDay, $books, $id) {
     $this->totalBooks = $totalBooks;
@@ -23,4 +23,13 @@ class Library {
     // echo "Libros:" . PHP_EOL;
     // print_r($this->books);
   }
+
+  public getBookScore() {
+    return array_reduce(array_filter(Problem::bookScores, function(book, index) {
+      return in_array(index, $this->books);
+    } ), function(sum, a) {
+      return sum + a;
+    }, 0)
+  }
 }
+// function que  calcule otra variable de la clase (bookScore)
